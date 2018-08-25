176e2753c669a6bae1d67d97d5ee03712fdce2d7
doremeez <doreme_crazy@hotmail.com>
2018-08-25 14:26:02 +0700

    update1

---
+++ app/src/main/java/masterung/androidthai/in/th/sutfriend/MyAlert.java
@@ -0,0 +1,30 @@
+package masterung.androidthai.in.th.sutfriend;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+public class MyAlert {
+
+    private Context context;
+
+    public MyAlert(Context context) {
+        this.context = context;
+    }
+
+    public void normalDialog(String titleString, String messageString) {
+
+        android.support.v7.app.AlertDialog.Builder builder = new android.support.v7.app.AlertDialog.Builder(context);
+        builder.setCancelable(false);
+        builder.setIcon(R.drawable.ic_action_alert);
+        builder.setTitle(titleString);
+        builder.setMessage(messageString);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+    }
+}
