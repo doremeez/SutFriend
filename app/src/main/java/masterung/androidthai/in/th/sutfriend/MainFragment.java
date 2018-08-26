77d3e3f76da00c53c410784c87fe1c3847425ff3
doremeez <doreme_crazy@hotmail.com>
2018-08-26 15:47:27 +0700

    rev1 26Aut

--- app/src/main/java/masterung/androidthai/in/th/sutfriend/MainFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/sutfriend/MainFragment.java
@@ -1,5 +1,6 @@
 package masterung.androidthai.in.th.sutfriend;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,7 +8,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainFragment extends Fragment{
 
@@ -17,8 +25,61 @@ public class MainFragment extends Fragment{
 
 //        Register Controller
         registerController();
+//        Check Login
+        checkLogin();
+
+//        Login Controller
+         loginController();
 
     }   // Main Method
+
+    private void loginController() {
+        Button button = getView().findViewById(R.id.btnLogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText emailEditText = getView().findViewById(R.id.edtUser);
+                EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+                String emailString = emailEditText.getText().toString().trim();
+                String passwordString = passwordEditText.getText().toString().trim();
+                final MyAlert myAlert = new MyAlert(getActivity());
+
+                if (emailString.isEmpty() || passwordString.isEmpty()) {
+                    myAlert.normalDialog("Have Space","Please Fill All Blank");
+                } else {
+
+                    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+                    firebaseAuth.signInWithEmailAndPassword(emailString, passwordString)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                    if (task.isSuccessful()) {
+                                        moveToService();
+                                    } else {
+                                        myAlert.normalDialog("Login Failure",task.getException().getMessage().toString());
+                                    } // if
+                                }
+                            });
+                }// if
+            }
+        });
+    }
+
+    private void checkLogin() {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        if (firebaseAuth.getCurrentUser() != null) {
+//            MoveTo Service
+
+            moveToService();
+        }
+    }
+
+    private void moveToService() {
+        startActivity(new Intent(getActivity(), ServiceActivity.class));
+        getActivity().finish();
+    }
 
 
     private void registerController() {
