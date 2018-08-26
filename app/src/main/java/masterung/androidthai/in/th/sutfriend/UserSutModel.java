6b223aa14429c7afed390b185156e8eb71d3af99
doremeez <doreme_crazy@hotmail.com>
2018-08-26 13:59:23 +0700

    rev1 26Aut

--- app/src/main/java/masterung/androidthai/in/th/sutfriend/UserSutModel.java
+++ app/src/main/java/masterung/androidthai/in/th/sutfriend/UserSutModel.java
@@ -1,24 +1,25 @@
 package masterung.androidthai.in.th.sutfriend;
 
+
 import android.os.Parcel;
 import android.os.Parcelable;
 
 public class UserSutModel implements Parcelable{
 
-    private String nameString, urlAvatarString;
+    private String nameString, urlAvataString;
 
     public UserSutModel() {
 
-    } //Constructor getter
+    }
 
-    public UserSutModel(String nameString, String urlAvatarString) {
+    public UserSutModel(String nameString, String urlAvataString) {
         this.nameString = nameString;
-        this.urlAvatarString = urlAvatarString;
-    } //Constructor setter
+        this.urlAvataString = urlAvataString;
+    }
 
     protected UserSutModel(Parcel in) {
         nameString = in.readString();
-        urlAvatarString = in.readString();
+        urlAvataString = in.readString();
     }
 
     public static final Creator<UserSutModel> CREATOR = new Creator<UserSutModel>() {
@@ -41,12 +42,12 @@ public class UserSutModel implements Parcelable{
         this.nameString = nameString;
     }
 
-    public String getUrlAvatarString() {
-        return urlAvatarString;
+    public String getUrlAvataString() {
+        return urlAvataString;
     }
 
-    public void setUrlAvatarString(String urlAvatarString) {
-        this.urlAvatarString = urlAvatarString;
+    public void setUrlAvataString(String urlAvataString) {
+        this.urlAvataString = urlAvataString;
     }
 
     @Override
@@ -57,6 +58,6 @@ public class UserSutModel implements Parcelable{
     @Override
     public void writeToParcel(Parcel parcel, int i) {
         parcel.writeString(nameString);
-        parcel.writeString(urlAvatarString);
+        parcel.writeString(urlAvataString);
     }
 }// Main Class
